771ac2a4333d74a1660d20f1e67ced914ed6dd94
Sam Kadmon <96024816+SamKadmon@users.noreply.github.com>
2024-10-15 12:29:05 +0200

    Animation Library

--- Week II/script.js
+++ Week II/script.js
@@ -2,25 +2,27 @@ import * as THREE from 'three';
 
 // Required: Scene + its meshes, Camera, Renderer
 const scene = new THREE.Scene();
+scene.fog = new THREE.Fog( 0xffffff, 0, 1 );
 
 function RandomNum(min, max)
 {
-    return Math.random() * (max - min) + min;
+    return Math.random() * (max -   min) + min;
 }
 
 // Mesh = Geometry + Material to fill in the gaps
 for (var i = 0; i < 1000; i++)
 {
-    const sphereGeometry = new THREE.SphereGeometry(0.02,32,32);
-    const sphereMaterial = new THREE.MeshBasicMaterial(
+    const cubeGeometry = new THREE.BoxGeometry(0.1,0.1,0.1);
+    const cubeMaterial = new THREE.MeshBasicMaterial(
         {
             color: 0xffffff,
             wireframe: false
         }
     );
-    const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
-    sphere.position.set(RandomNum(-10, 10),RandomNum(-5, 5),RandomNum(-5, 5));
-    scene.add(sphere);
+    const cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
+    cube.position.set(RandomNum(-5, 5),RandomNum(-5, 5),RandomNum(-5, 5));
+    cube.lookAt(new THREE.Vector3(0,0,0))
+    scene.add(cube);
 }
 
 // Camera
